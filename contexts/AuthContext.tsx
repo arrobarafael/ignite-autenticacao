939aeabe73109268fa027907143ff1319e076bc7
Rafael Pelegrini <arrobarafael@hotmail.com>
2021-06-15 12:09:17 -0300

    Configurado axios e feita parte inicial da rotina de SignIn

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode } from 'react';
+import { api } from '../src/services/api';
 
 type SignInCredentials = {
   email: string;
@@ -20,8 +21,16 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const isAuthenticated = false;
 
   async function signIn({ email, password }: SignInCredentials) {
-    console.log('signIn');
-    console.log({ email, password });
+    try {
+      const response = await api.post('sessions', {
+        email,
+        password,
+      });
+
+      console.log(response.data);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   return (
