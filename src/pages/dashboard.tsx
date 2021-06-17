8d6483dcb2ab7aadd3e6209929d81b421ded9eb4
Rafael Pelegrini <arrobarafael@hotmail.com>
2021-06-16 21:01:08 -0300

    Criado hook para tratar permissões de usuários

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
+import { useCan } from '../hooks/useCan';
 import { setupAPIClient } from '../services/api';
 import { api } from '../services/apiClient';
 import { withSSRAuth } from '../utils/withSSRAuth';
@@ -7,11 +8,20 @@ import { withSSRAuth } from '../utils/withSSRAuth';
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
 
+  const userCanSeeMetrics = useCan({
+    permissions: ['metrics.list'],
+  });
+
   useEffect(() => {
     api.get('/me').then((response) => console.log(response));
   }, []);
 
-  return <h1>dashboard {user?.email}</h1>;
+  return (
+    <>
+      <h1>dashboard {user?.email}</h1>
+      {userCanSeeMetrics && <div>Métricas</div>}
+    </>
+  );
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
